6a7a5d9f0dcccf01c08fb93c70022e3769f619b9
Oleg Borodin <oleg.borodin@pixel-networks.com>
2021-04-30 11:07:52 +0200

    added pseudo room controller and topics for view/control, see README

--- mqbot.go
+++ mqbot.go
@@ -9,10 +9,13 @@ import (
     "flag"
     "fmt"
     "os"
+    "encoding/json"
     "path/filepath"
     "log"
     "strconv"
     "time"
+    "strings"
+    "sync"
 
     "app/pmconfig"
     "app/pmtools"
@@ -22,12 +25,103 @@ import (
 
 type Application struct {
     config  *pmconfig.Config
+    trans   *transport.Transport
+    room    *Room
+}
+
+type Room struct {
+    Temp            int         `json:"temp"`
+    TempMtx         sync.Mutex  `json:"-"`
+    Light           int         `json:"light"`
+    LightMtx        sync.Mutex  `json:"-"`
+    Current         int         `json:"current"`
+    CurrentMtx      sync.Mutex  `json:"-"`
+
+    LightOn         bool        `json:"lightOn"`
+    LightOnMtx      sync.Mutex  `json:"-"`
+    CurrentOn       bool        `json:"currentOn"`
+    CurrentOnMtx    sync.Mutex  `json:"-"`
+}
+
+func NewRoom() *Room {
+    return &Room{
+        LightOn:        true,
+        CurrentOn:      true,
+    }
+}
+
+func (this *Room) Json() []byte {
+    this.LightOnMtx.Lock() 
+    this.CurrentOnMtx.Lock()
+    res, _ := json.Marshal(this)
+    this.LightOnMtx.Unlock()
+    this.CurrentOnMtx.Unlock()
+    return res
+}
+
+func (this *Room) GetLightOn() bool {
+    this.LightOnMtx.Lock()
+    defer this.LightOnMtx.Unlock()
+    return this.LightOn
+}
+
+func (this *Room) SetLightOn(value bool) {
+    this.LightOnMtx.Lock()
+    this.LightOn = value
+    this.LightOnMtx.Unlock()
+}
+
+func (this *Room) GetCurrentOn() bool {
+    this.CurrentOnMtx.Lock()
+    defer this.CurrentOnMtx.Unlock()
+    return this.CurrentOn
+}
+func (this *Room) SetCurrentOn(value bool) {
+    this.CurrentOnMtx.Lock()
+    this.CurrentOn = value
+    this.CurrentOnMtx.Unlock()
+}
+
+func (this *Room) GetLight() int {
+    this.LightMtx.Lock()
+    defer this.LightMtx.Unlock()
+    return this.Light
+}
+
+func (this *Room) SetLight(value int) {
+    this.LightMtx.Lock()
+    this.Light = value
+    this.LightMtx.Unlock()
+}
+
+func (this *Room) GetCurrent() int {
+    this.CurrentMtx.Lock()
+    defer this.CurrentMtx.Unlock()
+    return this.Current
+}
+func (this *Room) SetCurrent(value int) {
+    this.CurrentMtx.Lock()
+    this.Current = value
+    this.CurrentMtx.Unlock()
+}
+
+func (this *Room) GetTemp() int {
+    this.TempMtx.Lock()
+    defer this.TempMtx.Unlock()
+    return this.Temp
+}
+func (this *Room) SetTemp(value int) {
+    this.TempMtx.Lock()
+    this.Temp = value
+    this.TempMtx.Unlock()
 }
 
 func NewApplication() *Application {
     config := pmconfig.New()
+    room := NewRoom()
     return &Application{
         config:     config,
+        room:       room,
     }
 }
 
@@ -62,7 +156,7 @@ func (this *Application) Configure() {
     //}
 }
 
-func (this *Application) Run() error {
+func (this *Application) Start() error {
     var err error
     this.Configure()
 
@@ -75,6 +169,7 @@ func (this *Application) Run() error {
 			return err
 	}
 
+    this.config.Foreground = true
     daemon := pmdaemon.NewDaemon(
         this.config.MessageLogPath,
         this.config.PidPath,
@@ -88,29 +183,70 @@ func (this *Application) Run() error {
     pmdaemon.SetSignalHandler()
     
 	// Default operation
-    err = this.Loop()
+    err = this.Run()
     if err != nil {
         return err
     }
     return err
 }
 
-func (this *Application) Loop() error {
+func (this *Application) Run() error {
     var err error
 
-    trans := transport.New()
-    err = trans.Bind(this.config.Broker.Hostname,
+    this.trans = transport.New()
+    err = this.trans.Bind(this.config.Broker.Hostname,
             this.config.Broker.Port,
             this.config.Broker.Username,
             this.config.Broker.Password)
 
-	timer := time.NewTicker(1000 * time.Millisecond)
-	for time := range timer.C {
-            trans.Publish("/room1/current", strconv.Itoa(pmtools.GetRandomInt(1, 10)))
-            trans.Publish("/room1/temp", strconv.Itoa(pmtools.GetRandomInt(15, 25)))
-            trans.Publish("/room1/light", strconv.Itoa(pmtools.GetRandomInt(1000, 1500)))
-            log.Println("/time", time.String())
-            trans.Publish("/time", time.String())
+    lightOnFunc := func(topic string, payload []byte) {
+        log.Println("topic", topic, "payload", string(payload))
+        switch strings.ToLower(string(payload)) {
+            case "on":
+                this.room.SetLightOn(true)
+            case "off":
+                this.room.SetLightOn(false)
+        }
+    } 
+
+    currentOnFunc := func(topic string, payload []byte) {
+        log.Println("topic", topic, "payload", string(payload))
+        switch strings.ToLower(string(payload)) {
+            case "push":
+                if this.room.GetCurrentOn() {
+                    this.room.SetCurrentOn(false)
+                } else {
+                    this.room.SetCurrentOn(true)
+                }
+        }
+    } 
+
+    this.trans.Subscribe("/room1/currentOn", currentOnFunc)
+    this.trans.Subscribe("/room1/lightOn", lightOnFunc)
+    
+	timer := time.NewTicker(2000 * time.Millisecond)
+
+	for timeX := range timer.C {
+        
+            if this.room.GetCurrentOn() { 
+                this.room.SetCurrent(pmtools.GetRandomInt(10, 15))
+            } else {
+                this.room.SetCurrent(0)
+            }
+
+            if this.room.GetLightOn() {
+                this.room.SetLight(pmtools.GetRandomInt(1000, 1500))
+            } else {
+                this.room.SetLight(0)
+            }
+            this.room.SetTemp(pmtools.GetRandomInt(150, 250))
+
+            this.trans.Publish("/room1/current", strconv.Itoa(this.room.GetCurrent()))
+            this.trans.Publish("/room1/light", strconv.Itoa(this.room.GetLight()))
+            this.trans.Publish("/room1/temp", strconv.Itoa(this.room.GetTemp()))
+
+            this.trans.Publish("/room1/state", string(this.room.Json()))
+            this.trans.Publish("/time", timeX.String())
 	}
     return err
 }
@@ -127,7 +263,7 @@ func (this *Application) ShowConfig() {
 
 func main() {
     app := NewApplication()
-    err := app.Run()
+    err := app.Start()
     if err != nil {
         fmt.Fprintln(os.Stderr, "error:", err)
         os.Exit(1)
